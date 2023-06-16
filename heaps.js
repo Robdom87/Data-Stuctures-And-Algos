372d7009a9976374e135f84362b14f251717550b
Cedar Chavez <geggas00@gmail.com>
2023-06-15 18:31:20 -0700

    start notes

---
+++ heaps.js
@@ -0,0 +1,29 @@
+let notes = `
+another form of tree
+we will focus on binary heaps
+definition
+simialr to bst but
+Max tree, parent is greater than the cild
+min tree, parent nodes are always smaller,
+
+both of these are also as compact as possible, always takes least ampunt of space possible, left children always hilled out first
+no implied ordering between siblings, 
+
+the order does not have to do with left and right
+
+
+compare and contrast min and max heap
+implement max heasp
+understand where heaps are used 
+priority que using a heap
+very commony used data structure
+
+also used very much for graph traversal
+
+stroing heaps
+in a list or array
+for any indes of an array n, left child is stored at 2n+1, right is 2n+2
+
+to find parent (n-1)/2 - floor it, round to lowest whole num
+`
+
